9b5f2619ab0ff9fd4ee889da6f95f70af5d923c6
Adrian Martinez <afmartinez1215@gmail.com>
2023-04-20 23:02:15 -0400

    Building base classes

---
+++ graphs/graph_base.py
@@ -0,0 +1,20 @@
+import networkx
+import abc
+
+
+class graph(abc.ABC):
+
+    def __init__(self) -> None:
+        super().__init__()
+
+    def build_graph():
+        pass
+
+    def parent_graph():
+        pass
+
+    def _read_graph_yaml():
+        pass
+
+    def to_dot():
+        pass
